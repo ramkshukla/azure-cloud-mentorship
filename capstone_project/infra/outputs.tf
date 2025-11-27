454dafa3835714ff7f9df9e1af1e9f8c56aaf8a0
ramkshukla <ramkumarshukla00000@gmail.com>
2025-11-27 15:47:14 +0530

    added output

--- capstone_project/infra/outputs.tf
+++ capstone_project/infra/outputs.tf
@@ -13,3 +13,11 @@ output "keyvalut_uri" {
 output "log_analytics_ws_id" {
   value = azurerm_log_analytics_workspace.law.id
 }
+
+
+
+# Output
+keyvalut_uri = "https://kv-6624.vault.azure.net/"
+log_analytics_ws_id = "/subscriptions/ac6e8c49-833e-427d-bcc4-87042af5ddaf/resourceGroups/capstone-6624-rg/providers/Microsoft.OperationalInsights/workspaces/capstone-6624-law"
+resource_group_name = "capstone-6624-rg"
+static_website_url = "https://capstone6624sa.z13.web.core.windows.net/"
